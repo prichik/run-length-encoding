46b313c18de1866a95cd816b8b8858540565c9b4
prichik <eaprichinenko@edu.hse.ru>
2021-10-17 15:17:09 +0300

    Update run_length_encoding.py

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -1,4 +1,3 @@
-s = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
 def encode(s):
   s1 = ""
   countl = 0
@@ -8,38 +7,30 @@ def encode(s):
     countl += 1
     if s[i] != s[i+1]:
       qr_count = countl
-
-      s1 += str(qr_count) + s[i]
-
-      countl = 0
+      if qr_count == 1:
+          s1 += s[i]
+          countl = 0
+      else:
+          s1 += str(qr_count) + s[i]
+          countl = 0
   return s1
+  
+def decode(st):
+    news = ''
+    n = ''
+    for i in st:
+        if i.isdigit():
+            n += i
+        else:
+            if not n:
+                news += i
+            else:
+                news += i * int(n)
+                n = ''
+    return news
 
+s = input("Введите: ")
 s_chp1 = encode(s)
 print(s_chp1)
-
-def decode(s_chp):
-  s2 = ""
-  mn = []
-  m = []
-  mb = []
-  n = 0
-  sm = ""
-  for i in range(len(s_chp)):
-    if ord("A") <= ord(s_chp[i]) <= ord("Z"):
-      mb.append(s_chp[i])
-  for i in range(len(s_chp)):
-    if s_chp[i] in "1234567890":
-      s2 += s_chp[i]
-    else:
-      n = int(s2)
-      mn.append(n)
-      s2 = ""
-  for i in range(len(mn)):
-    m.append(mn[i])
-    m.append(mb[i])
-  for i in range(len(m)-1):
-    if i % 2 == 0:
-      sm += m[i] * m[i+1]
-  return sm
-
 print(decode(s_chp1))
+
